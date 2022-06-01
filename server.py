a2013d399dccaff039332eb8a327f7c6338a9d72
sclee0724 <sclee0724@gmail.com>
2022-06-01 22:20:49 +0900

    file edit

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 # -*- coding : utf=8 -*-
-from flask import Flask, render_template, request
+import io
+from flask import Flask, jsonify, render_template, request
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 app = Flask(__name__)
 
-@app.route("/")
+PATH = "./model/fin_classification_model.pt"
+
+torch.save(net.state_dict(), PATH)
+model = Net()
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+@app.route("/predict", method=['GET','POST'])
 def index():
-    return render_template('ch_project_web.html')
+    if request.method == 'GET':
+        return render_template('ch_project_web.html')
+#    if request.method == 'POST':
+
 
 host_addr = "0.0.0.0"
 port_num = "8080"
